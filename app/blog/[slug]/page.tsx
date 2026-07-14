44484540ec51b7555108b4f31c74e14b2e463360
Dileep Mali <dileepmali@amityonline.com>
2026-07-14 16:35:25 +0530

    final complate blog section

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -7,12 +7,66 @@ import { BlogContent } from "@/components/blog/blog-content";
 import { RelatedBlogs } from "@/components/blog/related-blogs";
 import { BlogTags } from "@/components/blog/blog-tags";
 import { ReadingProgress } from "@/components/blog/reading-progress";
+import { urlFor } from "@/sanity/lib/image";
+import { Metadata } from "next";
 
 interface Props {
   params: Promise<{
     slug: string;
   }>;
 }
+
+
+export async function generateMetadata({
+  params,
+}: Props): Promise<Metadata> {
+  const { slug } = await params;
+
+  const blog = await getBlogBySlug(slug);
+
+  if (!blog) {
+    return {
+      title: "Blog Not Found | Dilip OS",
+    };
+  }
+
+  const image =
+    blog.coverImage
+      ? urlFor(blog.coverImage).width(1200).height(630).url()
+      : "/og-image.png";
+
+  return {
+    title: `${blog.title} | Dilip OS`,
+    description: blog.excerpt,
+
+    openGraph: {
+      title: blog.title,
+      description: blog.excerpt,
+      type: "article",
+      url: `https://yourdomain.com/blog/${slug}`,
+      images: [
+        {
+          url: image,
+          width: 1200,
+          height: 630,
+          alt: blog.title,
+        },
+      ],
+    },
+
+    twitter: {
+      card: "summary_large_image",
+      title: blog.title,
+      description: blog.excerpt,
+      images: [image],
+    },
+
+    alternates: {
+      canonical: `https://yourdomain.com/blog/${slug}`,
+    },
+  };
+}
+
 
 export default async function BlogDetailsPage({ params }: Props) {
   const { slug } = await params;
